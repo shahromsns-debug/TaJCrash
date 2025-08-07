8fdae80fa408dd58dacb645736552f16471b82e6
shahromsns-debug <shahromsns@gmail.com>
2025-08-07 18:04:28 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+function makeBet() {
+    alert('Ставка сделана! (заглушка)');
+}
+function recharge() {
+    window.open('https://t.me/CryptoBot?start=crypto_pay_440515_AAY96bVPxmRJMesewJALR24lNrfaNaFC99Y', '_blank');
+}
+function withdraw() {
+    alert('Вывод доступен на Душанбе Сити и Alif (от 50 сомони)');
+}
+function openHistory() {
+    alert('Здесь будет история ваших ставок');
+}
+function openSupport() {
+    window.open('https://wa.me/992000307112', '_blank');
+}
